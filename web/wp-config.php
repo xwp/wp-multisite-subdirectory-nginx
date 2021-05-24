5de3a8c3fe81160c4b0b00f6ded7a5c51f192316
Kaspars Dambis <hi@kaspars.net>
2021-05-24 23:21:43 +0300

    Display errors to the frontend

--- web/wp-config.php
+++ web/wp-config.php
@@ -11,6 +11,7 @@ define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
 define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', true );
 
 define( 'MULTISITE', true );
 define( 'SUBDOMAIN_INSTALL', false );
